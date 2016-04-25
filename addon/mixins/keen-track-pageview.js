32f596062dbc53020fcba5a390699c8284c114b4
Francesco Novy <francesco@cropster.com>
2016-04-25 14:18:07 -0700

    Refactor keen-track-pageview to live in a function executed in beforeModel

--- addon/mixins/keen-track-pageview.js
+++ addon/mixins/keen-track-pageview.js
@@ -2,17 +2,46 @@ import Ember from 'ember';
 
 const { get } = Ember;
 
+/**
+ * A mixin to track page views with Keen.IO.
+ *
+ * @namespace EmberKeen.Mixin
+ * @class KeenTrackPageview
+ * @extends Ember.Mixin
+ * @public
+ */
 export default Ember.Mixin.create({
   keen: Ember.inject.service(),
 
+  /**
+   * Call the _trackPageView method on beforeModel.
+   *
+   * @method beforeModel
+   * @param {Transition} transition
+   * @override
+   * @returns {*}
+   * @public
+   */
   beforeModel(transition) {
+    this._trackPageView(transition);
+    return this._super(...arguments);
+  },
+
+  /**
+   * Track a page view for a given transition with Keen.IO.
+   *
+   * @method _trackPageView
+   * @param {Transition} transition The transition for the page view
+   * @private
+   */
+  _trackPageView(transition = {}) {
+    let keen = get(this, 'keen');
     let keenData = {
       page: get(transition, 'targetName'),
       queryParams: get(transition, 'queryParams')
     };
 
-    this.get('keen').sendEvent('page-view', keenData);
-    return this._super(...arguments);
+    keen.sendEvent('page-view', keenData);
   }
 
 });
